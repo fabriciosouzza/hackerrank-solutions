597834ebb602e65268ba20eea4b27a4696d005db
Fabricio Souza <fabriciossza@gmail.com>
2023-06-30 10:56:59 -0300

    fix: remove useless if-else statement of JumpingOnCloud

--- JumpingOnClouds.js
+++ JumpingOnClouds.js
@@ -7,14 +7,11 @@ function jumpingOnClouds(c, k) {
 
   do {
     i = ((i + k) % c.length)
-    if (i == 0) {
-      c[i] == 0 ? energy -= 1 : energy -= 3;
-      return energy;
-    } else {
-      c[i] == 0 ? energy -= 1 : energy -= 3;
-    }
+    c[i] == 0 ? energy -= 1 : energy -= 3;
   } while (i != 0);
-};
+
+    return energy;
+  };
 
 // function main() {
 // 	const arrClouds = [0, 0, 1, 0, 0, 1, 1, 0];
